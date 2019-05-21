c907599e82f7cc2e9f06139a8e8cf005c79e18a4
Dario Barać <dario.barac1@gmail.com>
2019-05-21 15:04:42 +0200

    add AssetService, Wallet, JuicEchain files

--- src/models/Asset.ts
+++ src/models/Asset.ts
@@ -14,17 +14,4 @@ export class Asset {
     public mediaType: string;
     public card: string;
     public issuer: string;
-
-    public parse(obj: Asset): void {
-        this.name = obj.name;
-        this.publisher = obj.publisher;
-        this.amount = obj.amount;
-
-        this.type = obj.type;
-        this.description = obj.description != undefined ? obj.description : "";
-
-        this.mediaUrl = obj.mediaUrl;
-        this.card = obj.card;
-        this.issuer = obj.issuer;
-    }
 }
---
+++ src/models/Wallet.ts
@@ -0,0 +1,9 @@
+export class Wallet {
+
+    public privateKey: string;
+    public publicKey: string;
+    public address: string;
+    public provider: string;
+    public title: string;
+    public node: string;
+}
