778d6acc8060bfbdb6e5a29b98522757c2481f17
Dario Barać <dario.barac1@gmail.com>
2019-05-21 15:05:16 +0200

    juicechain file

---
+++ src/JuicEchain.ts
@@ -0,0 +1,15 @@
+import { AssetService } from "./services/AssetService";
+
+export class JuicEchain {
+
+    private node: string;
+    private username: string;
+    private password: string;
+
+    //private walletService: WalletService;
+    private assetService: AssetService;
+    //private nodeService: NodeService;
+
+
+
+}
